e9560896fc3c4928ba2f243eb7ecddf2485368ff
skylarbsandler <skylarbsandler@gmail.com>
2023-08-03 10:58:34 -0600

    Commit All

---
+++ CaddyShackMVC/Controllers/ClubsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CaddyShackMVC.Models;
+using CaddyShackMVC.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+
+        public ClubsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Golfbags/{golfbagId:int}/clubs")]
+        public IActionResult Create(int golfbagId, Club club)
+        {
+            var golfbag = _context.GolfBags
+                .Where(g => g.Id == golfbagId)
+                .Include(g => g.Clubs)
+                .First();
+
+            golfbag.Clubs.Add(club);
+            _context.SaveChanges();
+
+            return RedirectToAction("show", new { golfbagId = golfbag.Id });
+        }
+
+        //[Route("/Golfbags/{id:int}/edit")]
+        //public IActionResult Edit(int id)
+        //{
+        //    var golfbag = _context.GolfBags.Find(id);
+
+        //    return View(golfbag);
+        //}
+
+        [Route("/Golfbags/{golfbagId:int}/edit")]
+        public IActionResult New(int golfbagId)
+        {
+            var golfbag = _context.GolfBags
+               .Where(g => g.Id == golfbagId)
+               .Include(g => g.Clubs)
+               .First();
+
+            return View(golfbag);
+        }
+    }
+}
---
+++ CaddyShackMVC/Controllers/GolfbagsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CaddyShackMVC.Models;
+using CaddyShackMVC.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class GolfbagsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+
+        public GolfbagsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var golfbags = _context.GolfBags;
+            return View(golfbags);
+        }
+
+        [Route("/Golfbags/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var golfbags = _context.GolfBags.Find(id);
+            return View(golfbags);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var golfbag = _context.GolfBags.Find(id);
+
+            _context.GolfBags.Remove(golfbag);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Golfbags/")]
+        public IActionResult Create(GolfBag golfbag)
+        {
+
+            _context.GolfBags.Add(golfbag);
+            _context.SaveChanges();
+
+            var newGolfbagId = golfbag.Id;
+
+            return RedirectToAction("show", new { id = newGolfbagId });
+        }
+
+        //[Route("/Golfbags/{id:int}/edit")]
+        //public IActionResult Edit(int id)
+        //{
+        //    var golfbag = _context.GolfBags.Find(id);
+
+        //    return View(golfbag);
+        //}
+    }
+}
